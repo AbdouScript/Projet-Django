036106c003a1738b4d669a442f3d056e19945ea5
ABDOU <abdoukichenama@gmail.com>
2022-04-22 16:26:40 +0200

    ajout des band et modification de la page /hello

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# ~/projects/django-web-app/merchex/listings/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes films préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
